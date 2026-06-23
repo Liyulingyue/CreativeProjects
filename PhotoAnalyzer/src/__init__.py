0265ac2809f735c768d4986e3869f2d96a73e87c
liyulingyue <852433440@qq.com>
2026-06-23 15:17:07 +0800

    feat: 添加 JSONL 格式支持，新增转换为 JSON 和 CSV 的功能，并实现分析结果的断点续传

--- PhotoAnalyzer/src/__init__.py
+++ PhotoAnalyzer/src/__init__.py
@@ -5,7 +5,7 @@ from .config import (
     is_image_file, get_image_files, ensure_dir,
 )
 from .analyzer import PhotoAnalyzer, BatchPhotoAnalyzer, AnalysisResult
-from .exporter import export_to_json, export_to_csv, print_summary
+from .exporter import export_to_json, export_to_csv, print_summary, convert_jsonl_to_json, convert_jsonl_to_csv
 
 __all__ = [
     "API_KEY", "BASE_URL", "MODEL_NAME",
@@ -14,4 +14,5 @@ __all__ = [
     "is_image_file", "get_image_files", "ensure_dir",
     "PhotoAnalyzer", "BatchPhotoAnalyzer", "AnalysisResult",
     "export_to_json", "export_to_csv", "print_summary",
+    "convert_jsonl_to_json", "convert_jsonl_to_csv",
 ]
